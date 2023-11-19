398172826e7237f40ee2cfe5f540eba760fd1d87
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2023-11-19 13:58:31 +0100

    adds icon to manifest

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix');
 mix.setPublicPath('./')
     .js('assets/js/content.js', 'dist/js').vue()
     .copy('assets/manifest.json', 'dist')
+    .copy('assets/images', 'dist/images')
     .options({
         processCssUrls: false
     });

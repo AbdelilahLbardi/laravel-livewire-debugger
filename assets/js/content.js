9f3c994404871b4ed804668786b0ca6c64648ef4
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2023-11-20 14:59:44 +0100

    instance vuejs only when the Livewire is present in the page

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from "./components/App.vue";
 
-const div = document.createElement("div")
-document.body.prepend(div);
+if (window.Livewire) {
+    const div = document.createElement("div")
+    document.body.prepend(div);
 
-new Vue({
-    el: div,
-    render: h => { return h(App); }
-});
+    new Vue({
+        el: div,
+        render: h => { return h(App); }
+    });
+}
